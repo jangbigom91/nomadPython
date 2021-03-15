5ae9c64993fe3202cbd1a10f0d1b73080e4e880b
jangbigom91 <jangbigom91@gmail.com>
2021-03-15 08:12:06 +0000

    21/3/15 indeed

---
+++ indeed.py
@@ -0,0 +1,37 @@
+# indeed 웹사이트 python페이지 Web Scrapping
+import requests
+from bs4 import BeautifulSoup
+
+LIMIT = 50
+URL = f"https://www.indeed.com/jobs?q=python&limit={LIMIT}"
+
+def extract_indeed_pages():
+  # requests로 url페이지 가져오기
+  result = requests.get(URL)
+
+  # bs4로 html 데이터 탐색
+  soup = BeautifulSoup(result.text, 'html.parser')
+
+  pagination = soup.find("div", {"class":"pagination"})
+
+  links = pagination.find_all('a')
+  pages = []
+
+  for link in links[:-1]:
+    pages.append(int(link.string)) # 페이지 수를 int로 변환
+
+  max_page = pages[-1] # 가장 마지막 페이지
+  return max_page
+
+def extract_indeed_jobs(last_page):
+  jobs = []
+  # for page in range(last_page):
+  result = requests.get(f"{URL}&start={0*LIMIT}")
+  soup = BeautifulSoup(result.text, 'html.parser')
+  results = soup.find_all("div", {"class":"jobsearch-SerpJobCard"})
+  
+  for result in results:
+    title = result.find("div", {"class":"title"})
+    print(title.find("a").string)
+    
+  return jobs
--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-# module 전체를 import 할수도 있다
-import math
+from indeed import extract_indeed_pages, extract_indeed_jobs
 
-print(math.fabs(-1.2))
+last_indeed_page = extract_indeed_pages()
 
-# 항상 사용할 것만 import하자
-# 특정 module만 import 할때
-from math import ceil, fsum
-
-print(ceil(1.2))
-print(fsum([1,2,3,4,5,6,7]))
-
-# import의 이름을 바꿔서 사용할 수도 있다
-from math import log10 as sexy_log10
-
-print(sexy_log10(100))
+indeed_jobs = extract_indeed_jobs(last_indeed_page)
