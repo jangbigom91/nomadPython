8a5df0df14187cd928334522637fae959e8ce4ff
jangbigom91 <jangbigom91@gmail.com>
2021-03-25 06:49:54 +0000

    21/3/25 extract company, location, link

--- indeed.py
+++ indeed.py
@@ -5,32 +5,55 @@ from bs4 import BeautifulSoup
 LIMIT = 50
 URL = f"https://www.indeed.com/jobs?q=python&limit={LIMIT}"
 
+
 def extract_indeed_pages():
-  # requests로 url페이지 가져오기
-  result = requests.get(URL)
+    # requests로 url페이지 가져오기
+    result = requests.get(URL)
 
-  # bs4로 html 데이터 탐색
-  soup = BeautifulSoup(result.text, 'html.parser')
+    # bs4로 html 데이터 탐색
+    soup = BeautifulSoup(result.text, 'html.parser')
 
-  pagination = soup.find("div", {"class":"pagination"})
+    pagination = soup.find("div", {"class": "pagination"})
 
-  links = pagination.find_all('a')
-  pages = []
+    links = pagination.find_all('a')
+    pages = []
 
-  for link in links[:-1]:
-    pages.append(int(link.string)) # 페이지 수를 int로 변환
+    for link in links[:-1]:
+        pages.append(int(link.string))  # 페이지 수를 int로 변환
 
-  max_page = pages[-1] # 가장 마지막 페이지
-  return max_page
+    max_page = pages[-1]  # 가장 마지막 페이지
+    return max_page
+
+
+def extract_job(html):
+    title = html.find("h2", {"class": "title"}).find("a")["title"]
+    company = html.find("div", {"class": "sjcl"}).find("span")
+    company_anchor = company.find("a")
+    if company_anchor is not None:
+        company = company_anchor.string
+    else:
+        company = company.string
+
+    location = html.find("div", {"class": "recJobLoc"})["data-rc-loc"]
+    job_id = html["data-jk"]
+
+    return {
+        'title': title,
+        'company': company,
+        'location': location,
+        "link": f"https://www.indeed.com/viewjob?jk={job_id}"
+    }
+
 
 def extract_indeed_jobs(last_page):
-  jobs = []
-  # for page in range(last_page):
-  result = requests.get(f"{URL}&start={0*LIMIT}")
-  soup = BeautifulSoup(result.text, 'html.parser')
-  results = soup.find_all("div", {"class":"jobsearch-SerpJobCard"})
-  
-  for result in results:
-    title = result.find("h2", {"class":"title"}).find("a")["title"]
-    print(title) # indeed 웹페이지 title scrapping
-  return jobs
+    jobs = []
+    for page in range(last_page):
+        print(f"Scrapping page {page}")
+        result = requests.get(f"{URL}&start={page*LIMIT}")
+        soup = BeautifulSoup(result.text, 'html.parser')
+        results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
+
+    for result in results:
+        job = extract_job(result)
+        jobs.append(job)
+    return jobs
