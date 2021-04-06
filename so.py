e4416419e281d5fea0e0561876fd11256995eeec
jangbigom91 <jangbigom91@gmail.com>
2021-04-06 06:10:26 +0000

    21/4/6 stackoverflow finish

--- so.py
+++ so.py
@@ -17,17 +17,19 @@ def extract_job(html):
     ["title"]
 
     company, location = html.find("h3", {
-        "class": "fc-black-700"
+        "class": "mb4"
     }).find_all("span", recursive=False)
 
-    print(company.get_text(strip=True), location.get_text(strip=True))
-    #0=False, 1=True
-    return {'title': title}
+    company = company.get_text(strip=True)
+    location = location.get_text(strip=True).strip("•").strip("\r").strip("\n")
+    job_id = html['data-jobid']
+    return {'title': title, 'company': company, 'location': location, "apply_link": f"https://stackoverflow.com/jobs/{job_id}"}
 
 
 def extract_jobs(last_page):
     jobs = []
     for page in range(last_page):
+        print(f"Scrapping SO: page: {page}")
         result = requests.get(f"{URL}&pg={page+1}")
         soup = BeautifulSoup(result.text, "html.parser")
         results = soup.find_all("div", {"class": "-job"})
